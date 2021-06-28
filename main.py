59473d80684746e2a0208760cd652549f0c80923
Mogly0815 <37794496+CheefBroady@users.noreply.github.com>
2021-06-28 19:15:34 +0000

    function prgram added

--- main.py
+++ main.py
@@ -1,48 +1,59 @@
 import art
 import game_data
+import clear
 from random import randint
 
 print(art.logo)
 print("Welcome to the higher lower game")
-print("Choose 'higher' or 'lower'")
+print("Choose 'higher' or 'lower'\n")
 
-def check_value(value, list):
+
+def equal_value(value, list):
   for x in list:
     if value == x:
       return True
-    else:
-      return False
+  return False
 
 def show_variante(my_list):
   comparisson = True
   while comparisson:
     idx = randint(0, len(game_data.data)-1)
-    comparisson = check_value(idx, my_list)
-    # for value in my_list:
-    #   if value == idx:
-    #     comparisson = False
-    #   else:
-    #     return
-    # comparisson = False
+    comparisson = equal_value(idx, my_list)
   my_list.append(idx)
   return idx
 
 
   
-  # my_list.append(idx)
-  # print(f"Index = {idx}")
-  # for value in my_list:
-  #   print(f"print listenvalues: {value}")
 
 
 def program():
-  game_data_compare_list = []
-  value_A = show_variante(game_data_compare_list)
-  value_B = show_variante(game_data_compare_list)
-  print(f"Variante A = {value_A}")
-  print(f"Variante B = {value_B}")
-  # for n in game_data_compare_list:
-  #   print(f"Value = {n}")
+  play_again = True
+  while play_again:
+    game_data_compare_list = []
+    idx_A = show_variante(game_data_compare_list)
+    idx_B = show_variante(game_data_compare_list)
+    for value in game_data.data[idx_A]:
+      print(f"{value}: {game_data.data[idx_A][value]}")
+    A = game_data.data[idx_A]['follower_count']
+    # print(f"follower_count: {A}")
+    print(f"{art.vs}\n")
+    for value in game_data.data[idx_B]:
+      if value != 'follower_count':
+        print(f"{value}: {game_data.data[idx_B][value]}")
+    B = game_data.data[idx_B]['follower_count']
+    # print(f"follower_count: {B}")
+    print(f"\nIs '{game_data.data[idx_B]['name']}' follower count higher or lower than '{game_data.data[idx_B]['name']}'?")
+    decission = input(f"\nChoose 'H' for 'Higher' or 'L' for 'Lower'\n")
+    if decission == 'H' and B > A:
+      print("*****You win, next round*****\n")
+    elif decission == 'L' and A > B:
+      print("*****You win, next round*****\n")
+    else:
+      play_again = False
+      print("You lose.")
+    
+
+
   
     
 program()

3c24fed88dc85f0d7ade2aa1c87edf8f60d2cfa0
Mogly0815 <37794496+CheefBroady@users.noreply.github.com>
2021-06-29 19:02:33 +0000

    game completed

--- main.py
+++ main.py
@@ -22,35 +22,46 @@ def show_variante(my_list):
   my_list.append(idx)
   return idx
 
-
-  
+'''
+Gewinn Meldung
+'''
+def you_win():
+  print("\n***************************************")
+  print("**********You Win, Next Round**********")
+  print("***************************************\n")
+ 
 
 
 def program():
+  counter = 0
   play_again = True
+  game_data_compare_list = []
+  idx_A = show_variante(game_data_compare_list)
+  game_data_compare_list.append(idx_A)
   while play_again:
-    game_data_compare_list = []
-    idx_A = show_variante(game_data_compare_list)
+    A = game_data.data[idx_A]['follower_count']
     idx_B = show_variante(game_data_compare_list)
     for value in game_data.data[idx_A]:
-      print(f"{value}: {game_data.data[idx_A][value]}")
-    A = game_data.data[idx_A]['follower_count']
-    # print(f"follower_count: {A}")
+      print(f"{value}: {game_data.data[idx_A][value]}")    
     print(f"{art.vs}\n")
     for value in game_data.data[idx_B]:
       if value != 'follower_count':
         print(f"{value}: {game_data.data[idx_B][value]}")
     B = game_data.data[idx_B]['follower_count']
+    print(f"Der Wert von B: {B}")
     # print(f"follower_count: {B}")
     print(f"\nIs '{game_data.data[idx_B]['name']}' follower count higher or lower than '{game_data.data[idx_B]['name']}'?")
     decission = input(f"\nChoose 'H' for 'Higher' or 'L' for 'Lower'\n")
+    idx_A = idx_B
     if decission == 'H' and B > A:
-      print("*****You win, next round*****\n")
+      counter += 1
+      you_win()
     elif decission == 'L' and A > B:
-      print("*****You win, next round*****\n")
+      counter += 1
+      you_win()
     else:
       play_again = False
-      print("You lose.")
+      print(f"You lose. Your score = {counter}")
     
 
 
